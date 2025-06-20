1e5e8dbb7b10a15f43a33ddc4608ea8991084468
Kerlin Michel <michelkerlin@gmail.com>
2025-06-20 13:20:34 -0700

    torchaudio_ctc_decoder_lexicon ml util func

--- kreyol_nlp/ml_utils/transformers.py
+++ kreyol_nlp/ml_utils/transformers.py
@@ -1,3 +1,5 @@
+from transformers.tokenization_utils import PreTrainedTokenizer
+
 from kreyol_nlp.alphabet import KREYÒL_ALPHABET, KREYÒL_UNIGRAPH_ALPHABET
 
 
@@ -12,3 +14,15 @@ def wav2vec_vocab(vocab: list,
 
 KREYÒL_WAV2VEC_VOCAB = wav2vec_vocab(KREYÒL_ALPHABET)
 KREYÒL_WAV2VEC_UNIGRAM_VOCAB = wav2vec_vocab(KREYÒL_UNIGRAPH_ALPHABET)
+
+def torchaudio_ctc_decoder_lexicon(words: set[str], tokenizer: PreTrainedTokenizer, file_path: str=None):
+    lexicon_words = []
+    for word in words:
+        tokenized_word = tokenizer.tokenize(word)
+        lexicon_words.append(f'{word} {" ".join(tokenized_word)} |')
+
+    if file_path != None:
+        with open(file_path, 'w+') as lexicon_file:
+            lexicon_file.write('\n'.join(lexicon_words))
+
+    return lexicon_words
