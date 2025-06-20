75f1f4dec9cbb9e19dffa52af197cfe90897de71
Kerlin Michel <michelkerlin@gmail.com>
2025-06-20 15:18:48 -0700

    ignore words with unknown tokens: torchaudio_ctc_decoder_lexicon

--- kreyol_nlp/ml_utils/transformers.py
+++ kreyol_nlp/ml_utils/transformers.py
@@ -1,4 +1,4 @@
-from transformers.tokenization_utils import PreTrainedTokenizer
+from transformers import Wav2Vec2CTCTokenizer
 
 from kreyol_nlp.alphabet import KREYÒL_ALPHABET, KREYÒL_UNIGRAPH_ALPHABET
 
@@ -15,10 +15,18 @@ def wav2vec_vocab(vocab: list,
 KREYÒL_WAV2VEC_VOCAB = wav2vec_vocab(KREYÒL_ALPHABET)
 KREYÒL_WAV2VEC_UNIGRAM_VOCAB = wav2vec_vocab(KREYÒL_UNIGRAPH_ALPHABET)
 
-def torchaudio_ctc_decoder_lexicon(words: set[str], tokenizer: PreTrainedTokenizer, file_path: str=None):
+def torchaudio_ctc_decoder_lexicon(words: set[str], tokenizer: Wav2Vec2CTCTokenizer, file_path: str=None):
+    unknown_token = tokenizer.special_tokens_map['unk_token']
+    unkown_token_id = tokenizer.encode(unknown_token)[0]
+
     lexicon_words = []
+
     for word in words:
         tokenized_word = tokenizer.tokenize(word)
+        e_tokenized_word = tokenizer.encode(tokenized_word)
+        # skip any words that has an unknown token
+        if unkown_token_id in e_tokenized_word:
+            continue
         lexicon_words.append(f'{word} {" ".join(tokenized_word)} |')
 
     if file_path != None:
